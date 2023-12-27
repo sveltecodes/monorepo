b7f87f8302cd785aa6375be5472d278f40717ef3
Matthew Davis <matthew@matthewdavis.io>
2023-12-27 12:31:12 -0500

    select no close modal

--- test/app/src/app.svelte
+++ test/app/src/app.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
+	import { ModalManager } from "@svelte.codes/modal-manager";
 	import "./app.pcss";
 	import Home from "./lib/components/home.svelte";
 	import ModalsManager from "./lib/components/modals-manager.svelte";
+
+	const manager = new ModalManager();
+
+	manager.open({
+		name: "test",
+		component: Home
+	});
 </script>
 
-<Home />
 <ModalsManager />
