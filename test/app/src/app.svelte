a4116bc37542fd80965a9a9c7c76dcbfd55647e5
mooy <61348580+mooyg@users.noreply.github.com>
2023-12-28 12:29:21 +0530

    fix: textarea component

--- test/app/src/app.svelte
+++ test/app/src/app.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import "./app.pcss";
 	import Home from "./lib/components/home.svelte";
-	import ModalsManager from "./lib/components/modals-manager.svelte";
 </script>
 
 <Home />
-<ModalsManager />
