2ca4164ba8c34f08c14161d2c287e2cacadf06aa
Matthew Davis <matthew@matthewdavis.io>
2024-02-20 20:28:08 -0500

    settings

--- test/app/src/lib/components/home.svelte
+++ test/app/src/lib/components/home.svelte
@@ -1,89 +1,86 @@
 <script lang="ts">
-	import "../../../../../packages/form-manager/public/styles/app.css";
-	import { Form, FormContainer, FormPasswordField, FormSelect, FormSwitch, FormTextField, email, minLength, FormTextarea } from "@svelte.codes/form-manager";
-	import ModalsManager from "./modals-manager.svelte";
-	import { Dialog } from "bits-ui";
+  import '../../../../../packages/form-manager/public/styles/app.css';
+  import { Form, FormContainer, FormPasswordField, FormSelect, FormSwitch, FormTextField, email, minLength, FormTextarea } from '@svelte.codes/form-manager';
+  import ModalsManager from './modals-manager.svelte';
+  import { Dialog } from 'bits-ui';
 
-	const form = new Form({
-		name: "test",
-		fields: {
-			email: {
-				name: "email",
-				label: "Email",
-				placeholder: "Email",
-				validators: [email]
-			},
-			firstName: {
-				name: "firstName",
-				label: "firstName",
-				placeholder: "firstName",
-				validators: [minLength(3)]
-			},
-			lastName: {
-				name: "lastName",
-				label: "lastName",
-				placeholder: "lastName"
-			},
-			password: {
-				name: "password",
-				label: "password",
-				placeholder: "password",
-				validators: [minLength(3)],
-				required: true
-			},
-		}
-	});
+  const form = new Form({
+    name: 'test',
+    fields: {
+      email: {
+        name: 'email',
+        label: 'Email',
+        placeholder: 'Email',
+        validators: [email]
+      },
+      firstName: {
+        name: 'firstName',
+        label: 'firstName',
+        placeholder: 'firstName',
+        validators: [minLength(3)]
+      },
+      lastName: {
+        name: 'lastName',
+        label: 'lastName',
+        placeholder: 'lastName'
+      },
+      password: {
+        name: 'password',
+        label: 'password',
+        placeholder: 'password',
+        validators: [minLength(3)],
+        required: true
+      }
+    }
+  });
 
-	$: errors = form.errors;
-	$: values = form.values;
- $:	console.log($errors)
+  $: errors = form.errors;
+  $: values = form.values;
+  $: console.log($errors);
 </script>
 
 <div>
-	values: {JSON.stringify($values)}
+  values: {JSON.stringify($values)}
 </div>
 <div>
-	errors: {JSON.stringify($errors)}
+  errors: {JSON.stringify($errors)}
 </div>
 <div />
 
 <Dialog.Root>
-	<Dialog.Trigger
-		class="inline-flex h-12 items-center text-black
-  justify-center whitespace-nowrap rounded-input bg-black px-[21px]
-  text-[15px] font-semibold shadow-mini transition-colors hover:bg-dark/95 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-foreground focus-visible:ring-offset-2 focus-visible:ring-offset-background active:scale-98"
-	>
-		New API key
-	</Dialog.Trigger>
-	<Dialog.Portal>
-		<Dialog.Overlay transitionConfig={{ duration: 150 }} class="fixed inset-0 z-50 bg-background/80 backdrop-blur-sm" />
-		<Dialog.Content class="fixed left-[50%] top-[50%] z-50 w-full max-w-[94%] translate-x-[-50%] translate-y-[-50%] rounded-card-lg border bg-background p-5 shadow-popover outline-none sm:max-w-[490px] md:w-full">
-			<Dialog.Title class="flex w-full items-center justify-center text-lg font-semibold tracking-tight">Create API key</Dialog.Title>
-			<Dialog.Description class="text-sm text-foreground-alt">Create and manage API keys. You can create multiple keys to organize your applications.</Dialog.Description>
-			<FormContainer onSubmit={(e) => {
-				e.preventDefault()
-				console.log(form.submit())
-			}} {form} classes="flex flex-col gap-2">
-				<FormTextField name="email" manager={form} placeholder="Email" />
-				<FormTextField name="firstName" manager={form} placeholder='First name'/>
-				<FormTextField placeholder='Last name' name="lastName" manager={form} />
-				<FormPasswordField name="password" manager={form} placeholder="Enter your password" />
-					<button
-					on:click={() => {
-						form.submit();
-						console.log("Errorrs", $errors);
-					}}>Submit</button
-				>
-			</FormContainer>
+  <Dialog.Trigger
+    class="rounded-input shadow-mini hover:bg-dark/95 focus-visible:ring-foreground
+  focus-visible:ring-offset-background active:scale-98 inline-flex h-12 items-center
+  justify-center whitespace-nowrap bg-black px-[21px] text-[15px] font-semibold text-black transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2">
+    New API key
+  </Dialog.Trigger>
+  <Dialog.Portal>
+    <Dialog.Overlay transitionConfig={{ duration: 150 }} class="bg-background/80 fixed inset-0 z-50 backdrop-blur-sm" />
+    <Dialog.Content class="rounded-card-lg bg-background shadow-popover fixed left-[50%] top-[50%] z-50 w-full max-w-[94%] translate-x-[-50%] translate-y-[-50%] border p-5 outline-none sm:max-w-[490px] md:w-full">
+      <Dialog.Title class="flex w-full items-center justify-center text-lg font-semibold tracking-tight">Create API key</Dialog.Title>
+      <Dialog.Description class="text-foreground-alt text-sm">Create and manage API keys. You can create multiple keys to organize your applications.</Dialog.Description>
+      <FormContainer
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(form.submit());
+        }}
+        {form}
+        classes="flex flex-col gap-2">
+        <FormTextField name="email" manager={form} placeholder="Email" />
+        <FormTextField name="firstName" manager={form} placeholder="First name" />
+        <FormTextField placeholder="Last name" name="lastName" manager={form} />
+        <FormPasswordField name="password" manager={form} placeholder="Enter your password" />
+        <button
+          on:click={() => {
+            form.submit();
+            console.log('Errorrs', $errors);
+          }}>Submit</button>
+      </FormContainer>
 
-			<div class="flex w-full justify-end">
-				<Dialog.Close
-					class="inline-flex h-input items-center justify-center rounded-input bg-dark px-[50px] text-[15px] font-semibold text-background shadow-mini hover:bg-dark/95 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-dark focus-visible:ring-offset-2 focus-visible:ring-offset-background active:scale-98"
-				>
-					Save
-				</Dialog.Close>
-			</div>
-		</Dialog.Content>
-	</Dialog.Portal>
+      <div class="flex w-full justify-end">
+        <Dialog.Close class="h-input rounded-input bg-dark text-background shadow-mini hover:bg-dark/95 focus-visible:ring-dark focus-visible:ring-offset-background active:scale-98 inline-flex items-center justify-center px-[50px] text-[15px] font-semibold focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2">Save</Dialog.Close>
+      </div>
+    </Dialog.Content>
+  </Dialog.Portal>
 </Dialog.Root>
 <ModalsManager />
--- test/app/src/lib/components/test-component.svelte
+++ test/app/src/lib/components/test-component.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import { ModalInstance } from "@svelte.codes/modal-manager";
 
-	export let instance: ModalInstance;
+	export let instance: ModalInstance<any>;
 </script>
 
 <div class="bg-slate-300 p-10">
 	<h1 class="text-3xl font-bold underline">this is inside a modal</h1>
-	<button on:click={() => instance.manager.close(instance.config.name)}>close</button>
+	<button on:click={() => instance.manager.close(instance.config.id)}>close</button>
 </div>
