3f9181ebe5e75d76642329495ebc92f52810581c
Matthew Davis <matthew@matthewdavis.io>
2024-02-29 15:58:20 -0500

    bump

--- test/app/src/lib/components/modals-manager.svelte
+++ test/app/src/lib/components/modals-manager.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-	import { ModalManager } from "@svelte.codes/modal-manager";
-	import TestComponent from "./test-component.svelte";
+  import { ModalManager } from '@svelte.codes/modal-manager';
+  import TestComponent from './test-component.svelte';
 
-	const manager = new ModalManager();
+  const manager = new ModalManager();
 
-	manager.open({
-		component: TestComponent,
-		classes: "bg-slate-300/50 p-10",
-		id: "test"
-	});
+  manager.open({
+    component: TestComponent,
+    classes: 'bg-slate-300/50 p-10',
+    id: 'test',
+    closeIcon: true
+  });
 </script>
 
 I'm inside a modal via modal manager!
